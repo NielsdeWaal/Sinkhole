3b82573415f11d2a0c91d8e2d50fec007490bbf6
Niels <nielsdwaal@gmail.com>
2022-04-29 22:20:10 +0200

    Working on writing larger value types to buffer

--- src/byte-stream.lisp
+++ src/byte-stream.lisp
@@ -20,6 +20,28 @@
 
 (deftype ub8 () '(unsigned-byte 8))
 
+(defun get-lsb-byte (number byte)
+  (declare (type integer number)
+           (type (signed-byte 32) byte))
+  (logand #xff (ash number (* byte -8))))
+
+(defun decode-lsb (bytes)
+  (let ((result 0))
+    (declare (type integer result))
+    (loop for b across bytes
+          for ix from 0 do
+            (setf result (logior result (ash b (* ix 8)))))
+    result))
+
+(defun encode-lsb (number bytes)
+  (declare (type integer number))
+  (let ((result (make-array (list bytes) :element-type 'ub8)))
+    (loop for x from 0 below bytes
+          do (setf (aref result x) (get-lsb-byte number x)))
+    result))
+
+(declaim (inline encode-lsb decode-lsb))
+
 (defclass byte-stream (fundamental-binary-stream)
   ((data
     :initarg :data
@@ -54,6 +76,16 @@
         (setf (aref (slot-value stream 'data) (slot-value stream 'position)) byte)
         (incf (slot-value stream 'position)))))
 
+(defmethod #+sbcl sb-gray:stream-write-sequence
+  #-sbcl stream-write-sequence ((stream byte-stream) sequence #+sbcl &optional start end #-sbcl &key)
+  (loop for ix from start to (or end (1- (length sequence)))
+        do (write-byte (aref sequence ix) stream)))
+
+;; (defmethod stream-write-sequence ((stream byte-stream) sequence start end
+;;                                   &key &allow-other-keys)
+;;   (with-slots (data) stream
+;;     (mapc #'(lambda (x) (write-byte x stream)) sequence)))
+
 (defmacro with-output-to-buffer ((var &key (size 4096)) &body body)
   `(let ((,var (make-byte-stream ,size)))
      ,@body
@@ -83,8 +115,11 @@
 ;;     (write-char #\a stream)))
 
 (defun testing ()
-  (let ((stream (make-byte-stream 10)))
-    (write-byte 42 stream)
-    (write-byte 42 stream)
-    (write-byte 42 stream)
-    stream))
+  (with-output-to-buffer (stream :size 10)
+    (write-sequence (encode-lsb 63550 2) stream :end 1))
+  ;; (let ((stream (make-byte-stream 10)))
+  ;;   (write-byte 42 stream)
+  ;;   (write-byte 42 stream)
+  ;;   (write-byte 42 stream)
+  ;;   stream)
+  )
